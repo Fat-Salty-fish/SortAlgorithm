c1e395ccbb068c04bad68503c3b4eba09156239f
李忠杰 <lizhongjie@koolearn.com>
2020-02-06 00:04:47 +0800

    完成了leetcode24题

--- src/leetCode/LeetCode24两两交换链表中的节点.java
+++ src/leetCode/LeetCode24两两交换链表中的节点.java
@@ -8,30 +8,30 @@ package leetCode;
 public class LeetCode24两两交换链表中的节点 {
 
     public ListNode swapPairs(ListNode head) {
-        ListNode dummyNode = new ListNode(0);
-        ListNode result = new ListNode(0);
+        ListNode dummyNode = new ListNode(-1);
+        ListNode result = dummyNode;
         dummyNode.next = head;
-        ListNode node1 = dummyNode.next;
-        ListNode node2 = dummyNode.next.next;
-
-        swap(dummyNode,node1,node2);
-        dummyNode.next = dummyNode.next.next.next;
-        result.next = dummyNode.next;
-        node1 = dummyNode.next;
-        node2 = dummyNode.next.next;
-        while (node1 != null && node2 != null) {
-            swap(dummyNode, node1, node2);
-            dummyNode.next = dummyNode.next.next.next;
-            node1 = dummyNode.next;
-            node2 = dummyNode.next.next;
-        }
+        swap(dummyNode);
         return result.next;
     }
 
-    public void swap(ListNode dummyNode, ListNode node1, ListNode node2) {
+    public void swap(ListNode dummyNode) {
+        if (dummyNode == null) {
+            return;
+        }
+        ListNode node1 = dummyNode.next;
+        if (node1 == null) {
+            return;
+        }
+        ListNode node2 = node1.next;
+        if(node2 == null){
+            return;
+        }
         dummyNode.next = node2;
         node1.next = node2.next;
         node2.next = node1;
+        dummyNode = dummyNode.next.next;
+        swap(dummyNode);
     }
 
     public static void main(String[] args) {
@@ -40,13 +40,16 @@ public class LeetCode24两两交换链表中的节点 {
         ListNode node3 = new ListNode(3);
         ListNode node4 = new ListNode(4);
         ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
-        node1.next=node2;
-        node2.next=node3;
-        node3.next=node4;
-        node4.next=node5;
-        node5.next=node6;
-
-
+//        ListNode node6 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+//        node5.next = node6;
+        ListNode temp = new LeetCode24两两交换链表中的节点().swapPairs(null);
+        while (temp!=null){
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
     }
 }
