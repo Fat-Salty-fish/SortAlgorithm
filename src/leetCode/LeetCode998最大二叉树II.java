2f4e83a65cd45119d009a71d273577e3a4228d79
李忠杰 <lizhongjie@koolearn-inc.com>
2021-08-09 22:51:52 +0800

    LeetCode998 complete

--- src/leetCode/LeetCode998最大二叉树II.java
+++ src/leetCode/LeetCode998最大二叉树II.java
@@ -8,12 +8,24 @@ package leetCode;
 public class LeetCode998最大二叉树II {
 
     /**
-     * 
+     * 如果val比root大 则root直接作为val为根的节点的左子节点
+     * 如果val比root小 则需要将val插入到root的右子节点的合适位置中
+     *
      * @param root
      * @param val
      * @return
      */
     public TreeNode insertIntoMaxTree(TreeNode root, int val) {
-        return null;
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            TreeNode newHead = new TreeNode(val);
+            newHead.left = root;
+            return newHead;
+        } else {
+            root.right = insertIntoMaxTree(root.right, val);
+            return root;
+        }
     }
 }
