234e9cba720b384de1183b1977074248fa8810b4
李忠杰 <lizhongjie@koolearn-inc.com>
2021-11-01 16:30:25 +0800

    完成二刷322

--- src/leetCode/LeetCode322零钱兑换.java
+++ src/leetCode/LeetCode322零钱兑换.java
@@ -22,7 +22,7 @@ public class LeetCode322零钱兑换 {
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int j = 0;j<coins.length;j++){
+            for (int j = 0; j < coins.length; j++) {
                 if (coins[j] <= i) {
                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
@@ -31,9 +31,34 @@ public class LeetCode322零钱兑换 {
         return dp[amount] > amount ? -1 : dp[amount];
     }
 
+    /**
+     * 动态规划 二刷
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange2(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        // 用于检查是否更新了结果
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j]) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
     public static void main(String[] args) {
-        int[] coins = {1,2,5};
+        int[] coins = {1, 2, 5};
         int amount = 11;
-        int result = new LeetCode322零钱兑换().coinChange(coins,amount);
+        int result = new LeetCode322零钱兑换().coinChange(coins, amount);
     }
 }
